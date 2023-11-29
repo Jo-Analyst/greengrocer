962106231782d70d08645cb0553f99185cf5cbff
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2023-11-29 12:00:31 -0300

    Adicionando o formulário, botão de cadastro e rolagem

--- lib/src/auth/components/sign_up_screen.dart
+++ lib/src/auth/components/sign_up_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:greengrocer/src/auth/components/custom_text_field.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
@@ -29,6 +30,47 @@ class SignUpScreen extends StatelessWidget {
               borderRadius: BorderRadius.vertical(
                 top: Radius.circular(45),
               ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const CustomTextField(
+                  icon: Icons.email,
+                  label: "Email",
+                ),
+                const CustomTextField(
+                  icon: Icons.lock,
+                  label: "Senha",
+                  isSecret: true,
+                ),
+                const CustomTextField(
+                  icon: Icons.person,
+                  label: "Nome",
+                ),
+                const CustomTextField(
+                  icon: Icons.phone_android,
+                  label: "Celular",
+                ),
+                const CustomTextField(
+                  icon: Icons.file_copy,
+                  label: "CPF",
+                ),
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "Cadastrar Usuário",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
