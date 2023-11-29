9ff90ad1558e023fad4ea2ed993a881a66bdd098
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2023-11-29 14:15:55 -0300

    14. Adicionando o formulário, botão de cadastro e rola

--- lib/src/auth/components/sign_up_screen.dart
+++ lib/src/auth/components/sign_up_screen.dart
@@ -7,73 +7,78 @@ class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Expanded(
-            child: Center(
-              child: Text(
-                "Cadastro",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 35,
-                ),
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 42,
-              vertical: 40,
-            ),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(45),
-              ),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const CustomTextField(
-                  icon: Icons.email,
-                  label: "Email",
-                ),
-                const CustomTextField(
-                  icon: Icons.lock,
-                  label: "Senha",
-                  isSecret: true,
-                ),
-                const CustomTextField(
-                  icon: Icons.person,
-                  label: "Nome",
-                ),
-                const CustomTextField(
-                  icon: Icons.phone_android,
-                  label: "Celular",
-                ),
-                const CustomTextField(
-                  icon: Icons.file_copy,
-                  label: "CPF",
-                ),
-                SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18),
-                      ),
-                    ),
-                    onPressed: () {},
-                    child: const Text(
-                      "Cadastrar Usuário",
-                      style: TextStyle(fontSize: 18),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+              const Expanded(
+                child: Center(
+                  child: Text(
+                    "Cadastro",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 42,
+                  vertical: 40,
+                ),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(45),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const CustomTextField(
+                      icon: Icons.email,
+                      label: "Email",
+                    ),
+                    const CustomTextField(
+                      icon: Icons.lock,
+                      label: "Senha",
+                      isSecret: true,
+                    ),
+                    const CustomTextField(
+                      icon: Icons.person,
+                      label: "Nome",
+                    ),
+                    const CustomTextField(
+                      icon: Icons.phone_android,
+                      label: "Celular",
+                    ),
+                    const CustomTextField(
+                      icon: Icons.file_copy,
+                      label: "CPF",
+                    ),
+                    SizedBox(
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          "Cadastrar Usuário",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
