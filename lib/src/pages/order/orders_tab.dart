530ac0f5e012316a72b72d16c6ccab717a3d16d4
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2023-12-05 14:46:21 -0300

    39. Iniciando o desenvolvimento e criando mock

---
+++ lib/src/pages/order/orders_tab.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class OrdersTab extends StatelessWidget {
+  const OrdersTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pedidos"),
+      ),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(16),
+        physics: const BouncingScrollPhysics(),
+        itemBuilder: (_, index) {
+          return Text("data- $index");
+        },
+        separatorBuilder: (_, __) => const SizedBox(height: 10),
+        itemCount: 2,
+      ),
+    );
+  }
+}
