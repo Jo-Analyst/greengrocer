b141e352eb8ff25f7fe778dee501b069a74322b6
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2023-12-05 11:49:54 -0300

    38. Dialog para atualizar senha

--- lib/src/pages/profile/profile_tab.dart
+++ lib/src/pages/profile/profile_tab.dart
@@ -2,8 +2,90 @@ import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/app_data.dart' as app_data;
 import 'package:greengrocer/src/pages/common_widgets/custom_text_field.dart';
 
-class ProfileTab extends StatelessWidget {
+class ProfileTab extends StatefulWidget {
   const ProfileTab({super.key});
+
+  @override
+  State<ProfileTab> createState() => _ProfileTabState();
+}
+
+class _ProfileTabState extends State<ProfileTab> {
+  Future<bool?> updateProfile() {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 12),
+                      child: Text(
+                        "Atualização de senha",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    // Senha
+                    const CustomTextField(
+                      isSecret: true,
+                      icon: Icons.lock,
+                      label: "Senha atual",
+                    ),
+                    // Nova Senha
+                    const CustomTextField(
+                      isSecret: true,
+                      icon: Icons.lock_outline,
+                      label: "Nova Senha",
+                    ),
+                    // Confirmação da senha
+                    const CustomTextField(
+                      isSecret: true,
+                      icon: Icons.lock_outline,
+                      label: "Confirmar nova senha",
+                    ),
+                    // Botão de confirmação
+                    SizedBox(
+                      height: 45,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: const Text("Atualizar"),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: 5,
+                right: 5,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(Icons.close),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,7 +146,9 @@ class ProfileTab extends StatelessWidget {
                   borderRadius: BorderRadius.circular(20),
                 ),
               ),
-              onPressed: () {},
+              onPressed: () {
+                updateProfile();
+              },
               child: const Text("Atualizar Senha"),
             ),
           ),
