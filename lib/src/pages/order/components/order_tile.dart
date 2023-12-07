145580cb629565c9f41990b0eac1473191b1b11e
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2023-12-07 14:49:38 -0300

    45. Total e botão de pagamento

--- lib/src/pages/order/components/order_tile.dart
+++ lib/src/pages/order/components/order_tile.dart
@@ -33,6 +33,7 @@ class OrderTile extends StatelessWidget {
             ],
           ),
           childrenPadding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+          expandedCrossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             IntrinsicHeight(
               child: Row(
@@ -70,6 +71,42 @@ class OrderTile extends StatelessWidget {
                     ),
                   )
                 ],
+              ),
+            ),
+
+            // Total
+            Text.rich(
+              TextSpan(
+                style: const TextStyle(fontSize: 20),
+                children: [
+                  const TextSpan(
+                    text: "Total ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(
+                    text: UtilsServices.priceToCurrency(order.total),
+                  ),
+                ],
+              ),
+            ),
+
+            // Botão de pagamento
+            Visibility(
+              visible: order.status == "pending_payment",
+              child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.pix,
+                  size: 18,
+                ),
+                label: const Text("Ver QR Code Pix"),
               ),
             ),
           ],
