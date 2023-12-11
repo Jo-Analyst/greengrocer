2882edf077b31a98f9c2d684b31fb0a991003a8f
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-12-11 05:29:11 -0300

    55. Adicionando ícone customizado na aplicação

--- lib/src/config/app_data.dart
+++ lib/src/config/app_data.dart
@@ -6,14 +6,14 @@ import 'package:greengrocer/src/models/user_model.dart';
 ItemModel apple = ItemModel(
   description:
       'A melhor maçã da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
-  imgUrl: 'assets/images/fruits/apple.png',
+  imgUrl: 'assets/fruits/apple.png',
   itemName: 'Maçã',
   price: 5.5,
   unit: 'kg',
 );
 
 ItemModel grape = ItemModel(
-  imgUrl: 'assets/images/fruits/grape.png',
+  imgUrl: 'assets/fruits/grape.png',
   itemName: 'Uva',
   price: 7.4,
   unit: 'kg',
@@ -22,7 +22,7 @@ ItemModel grape = ItemModel(
 );
 
 ItemModel guava = ItemModel(
-  imgUrl: 'assets/images/fruits/guava.png',
+  imgUrl: 'assets/fruits/guava.png',
   itemName: 'Goiaba',
   price: 11.5,
   unit: 'kg',
@@ -31,7 +31,7 @@ ItemModel guava = ItemModel(
 );
 
 ItemModel kiwi = ItemModel(
-  imgUrl: 'assets/images/fruits/kiwi.png',
+  imgUrl: 'assets/fruits/kiwi.png',
   itemName: 'Kiwi',
   price: 2.5,
   unit: 'un',
@@ -40,7 +40,7 @@ ItemModel kiwi = ItemModel(
 );
 
 ItemModel mango = ItemModel(
-  imgUrl: 'assets/images/fruits/mango.png',
+  imgUrl: 'assets/fruits/mango.png',
   itemName: 'Manga',
   price: 2.5,
   unit: 'un',
@@ -49,7 +49,7 @@ ItemModel mango = ItemModel(
 );
 
 ItemModel papaya = ItemModel(
-  imgUrl: 'assets/images/fruits/papaya.png',
+  imgUrl: 'assets/fruits/papaya.png',
   itemName: 'Mamão papaya',
   price: 8,
   unit: 'kg',
